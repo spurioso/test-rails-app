5fabd84f2d03f89fe4e5d64d1e52ea7f4e66625f
spurioso <spurioso@gmail.com>
2013-11-17 16:39:40 -0500

    put confi.assets.initialize line in the right place

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,13 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-# Prevent Heroku from trying to access database on deployment
-config.assets.initialize_on_precompile = false
 
 module TestApp
   class Application < Rails::Application
+   
+    # Prevent Heroku from trying to access database on deployment
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
