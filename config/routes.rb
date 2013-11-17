ab5b4ba1a0a608ad116dc71a4f20cab9b4731919
spurioso <spurioso@gmail.com>
2013-11-17 16:59:09 -0500

    Fixed syntax in routes.rb (need hash notation)

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ TestApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-    root 'users#index'
+    root :to => 'users#index'
 
   # See how all your routes lay out with "rake routes"
 
